fed69895728b395f4538d1f1e3e176e66f5ebd5a
Sunil Targe <suniltarge@gmail.com>
2019-10-05 16:11:42 +0800

    first commit

---
+++ BaseApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaseApp
+//
+//  Created by Sunil Targe on 2019/10/5.
+//  Copyright © 2019 suniltarge.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
